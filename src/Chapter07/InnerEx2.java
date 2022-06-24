753d26603a5f2e7d1c92d5b9fb7f10d286aba84a
Jaeho_Jeong <88024190+wogh0013@users.noreply.github.com>
2022-06-24 22:53:29 +0900

    Update InnerEx2.java

--- src/Chapter07/InnerEx2.java
+++ src/Chapter07/InnerEx2.java
@@ -6,7 +6,7 @@ public class InnerEx2 {
     }; //익명 클래스(선언과 생성을 동시에)
 
     static Object cv = new Object(){
-        void method(){};
+        void method(){}; 
     }; //익명 클래스
 
     void myMethod(){
